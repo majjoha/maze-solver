20d893e4c11518ce94255f62da97ab5015393b6d
Mathias Jean Johansen <mathias@mjj.io>
2019-03-14 12:14:42 +0100

    Remove line break.

--- lib/maze_solver/one_point_crossover.rb
+++ lib/maze_solver/one_point_crossover.rb
@@ -7,7 +7,6 @@ module MazeSolver
       second_parent:,
       crossover_point: Random.rand(Chromosome::GENE_LENGTH)
     )
-
       Chromosome.new(
         genes: first_parent.genes[0...crossover_point].concat(
           second_parent.genes[crossover_point..Chromosome::GENE_LENGTH]
